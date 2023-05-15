153682364840c915a099e94cab73512a617ac792
erikjohns <erikj3520@gmail.com>
2023-05-15 20:19:51 +0200

    Simplifying Header

--- app/src/main/java/com/example/smartycats/MainActivity.java
+++ app/src/main/java/com/example/smartycats/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.smartycats;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+
+import android.graphics.Typeface;
 import android.os.Bundle;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
