740891b49063e673ca606cfeedbbd065dfad7e5d
erikjohns <erikj3520@gmail.com>
2023-05-16 12:38:16 +0200

    Updated Menu Icons

--- app/src/main/java/com/example/smartycats/MainActivity.java
+++ app/src/main/java/com/example/smartycats/MainActivity.java
@@ -1,20 +1,15 @@
 package com.example.smartycats;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.SearchView;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
-
-import android.graphics.Typeface;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.google.android.material.navigation.NavigationView;
 
@@ -38,18 +33,16 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         // Sidebar Menu
         navigationView.setNavigationItemSelectedListener(this);
-        openButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(drawerLayout.isDrawerOpen(GravityCompat.END)) {
-                    drawerLayout.closeDrawer(GravityCompat.END);
-                }
-                drawerLayout.openDrawer(GravityCompat.END);
+        openButton.setOnClickListener(view -> {
+            if(drawerLayout.isDrawerOpen(GravityCompat.END)) {
+                drawerLayout.closeDrawer(GravityCompat.END);
             }
+            drawerLayout.openDrawer(GravityCompat.END);
         });
 
     }
 
+    @SuppressLint("NonConstantResourceId")
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
         switch (menuItem.getItemId()) {
