4c6534ce6def55b57b2634caedbf20038d4c4d1e
erikjohns <erikj3520@gmail.com>
2023-05-16 11:41:41 +0200

    Creating Sidebar Menu

--- app/src/main/java/com/example/smartycats/MainActivity.java
+++ app/src/main/java/com/example/smartycats/MainActivity.java
@@ -1,18 +1,68 @@
 package com.example.smartycats;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-public class MainActivity extends AppCompatActivity {
+    // Variables
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Button openButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Hooks
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigation_view);
+        openButton = findViewById(R.id.open_button);
+
+        // Sidebar Menu
+        navigationView.setNavigationItemSelectedListener(this);
+        openButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(drawerLayout.isDrawerOpen(GravityCompat.END)) {
+                    drawerLayout.closeDrawer(GravityCompat.END);
+                }
+                drawerLayout.openDrawer(GravityCompat.END);
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.nav_home:
+                Toast.makeText(MainActivity.this, "Home", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_assignments:
+                Toast.makeText(MainActivity.this, "Assignments", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                Toast.makeText(MainActivity.this, "Nothing.", Toast.LENGTH_SHORT).show();
+
+        }
+        return false;
     }
 }
